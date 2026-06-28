0ebe5c4c4e34a6a600807abbe7fa50e6a33eaafb
kanema06 <karennegrin853@gmail.com>
2026-06-28 11:08:11 -0400

    paint command changed

--- minix/commands/paint/paint.c
+++ minix/commands/paint/paint.c
@@ -2,69 +2,71 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <termios.h>
 
-#define SCREEN_WIDTH 80
-#define SCREEN_HEIGHT 25
-
-#define CURSOR_CHAR 'O'
-#define BRUSH_CHAR  '#'
-#define ERASER_CHAR ' '
+#define FILAS 24
+#define COLS 80
 
 int main() {
-    int fd = open("/dev/mouse0", O_RDONLY);
-    if (fd < 0) {
-        perror("Error: No se pudo abrir /dev/mouse0. Revisa tu driver");
-        return 1;
-    }
-
-    struct termios old_tio, new_tio;
-    tcgetattr(STDIN_FILENO, &old_tio);
-    new_tio = old_tio;
-    new_tio.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
-
-    printf("\033[2J");
-
-    int x = SCREEN_WIDTH / 2;
-    int y = SCREEN_HEIGHT / 2;
-
-    unsigned char packet[3];
+    int fd;
+    unsigned char paquete[3];
+    char pantalla[FILAS][COLS];
     
-    while (1) {
-        printf("\033[%d;%dH%c", y, x, CURSOR_CHAR);
-        fflush(stdout);
-
-        int bytes = read(fd, packet, 3);
-        
-        if (bytes == 3) {
-
-            int left_click = packet[0] & 0x01;
-            int right_click = packet[0] & 0x02;
-
-            char dx = (char)packet[1]; 
-            char dy = (char)packet[2]; 
-            
-            printf("\033[%d;%dH", y, x);
-            if (left_click) {
-                printf("%c", BRUSH_CHAR); 
-            } else if (right_click) {
-                printf("%c", ERASER_CHAR); 
-            } else {
-                printf(" "); 
-            }
-
-            x += dx;
-            y -= dy;
-
-            if (x < 1) x = 1;
-            if (x > SCREEN_WIDTH) x = SCREEN_WIDTH;
-            if (y < 1) y = 1;
-            if (y > SCREEN_HEIGHT) y = SCREEN_HEIGHT;
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            pantalla[i][j] = ' ';
         }
     }
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+    printf("\033[2J");
+    
+    int x = COLS / 2;
+    int y = FILAS / 2;
+
+    fd = open("/dev/mouse0", O_RDONLY);
+    if (fd < 0) {
+        perror("Error al abrir /dev/mouse0");
+        return 1;
+    }
+
+    printf("\033[%d;%dH+", y + 1, x + 1);
+    fflush(stdout);
+
+    while (1) {
+        if (read(fd, paquete, 3) == 3) {
+            
+            int click_izq = paquete[0] & 0x01;
+            int click_der = (paquete[0] & 0x02) >> 1;
+            
+            int dx = (signed char)paquete[1];
+            int dy = (signed char)paquete[2];
+
+            printf("\033[%d;%dH%c", y + 1, x + 1, pantalla[y][x]);
+
+            if (click_izq) {
+                pantalla[y][x] = '#'; 
+                printf("\033[%d;%dH#", y + 1, x + 1); 
+            } else if (click_der) {
+                pantalla[y][x] = ' '; 
+                printf("\033[%d;%dH ", y + 1, x + 1); 
+            }
+
+            int nuevo_x = x + (dx / 4);
+            int nuevo_y = y - (dy / 4); 
+
+            if (nuevo_x < 0) nuevo_x = 0;
+            if (nuevo_x >= COLS) nuevo_x = COLS - 1;
+            if (nuevo_y < 0) nuevo_y = 0;
+            if (nuevo_y >= FILAS) nuevo_y = FILAS - 1;
+
+            x = nuevo_x;
+            y = nuevo_y;
+
+            
+            printf("\033[%d;%dH+", y + 1, x + 1);
+            fflush(stdout);
+        }
+    }
+
     close(fd);
     return 0;
 }
